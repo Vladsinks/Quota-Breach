529fc28dcc6183618fcae855a1a33af9f288c363
Vlad <Vladsinks7@gmail.com>
2026-05-01 22:40:22 +0300

    ф
    ф

--- Quota Breach/Assets/Test67/Scripts/RoomVariants.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class RoomVariants : MonoBehaviour
-{
-    public GameObject[] bottomRooms;
-    public GameObject[] leftRooms;
-    public GameObject[] topRooms;
-    public GameObject[] rightRooms;
-}
